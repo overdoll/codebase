390cca845f8ac583e49d82c64ee30dab1ea79459
nikita-tkachov <nikita.tkachov@protonmail.com>
2020-12-24 15:32:26 -0500

    adding all other operations

--- applications/eva/server/server.go
+++ applications/eva/server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/gocqlx/v2/qb"
+	"github.com/scylladb/gocqlx/v2/table"
 	"log"
 	"net"
 	"os"
@@ -30,6 +31,11 @@ type AuthenticationCookie struct {
 	Email      string
 	Redeemed   bool
 	Expiration time.Time
+}
+
+type RegisteredUser struct {
+	Username string
+	Email    string
 }
 
 var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
@@ -63,7 +69,7 @@ func (s *Server) Run() {
 	}()
 }
 
-func (s *Server) CreateAuthenticationCookie(ctx context.Context, session gocqlx.Session, request *evav1.GetAuthenticationCookieRequest) (*evav1.GetAuthenticationCookieResponse, error) {
+func (s *Server) CreateAuthenticationCookie(ctx context.Context, session gocqlx.Session, request *evav1.CreateAuthenticationCookieRequest) (*evav1.CreateAuthenticationCookieResponse, error) {
 	if request == nil || request.Email == "" {
 		return nil, fmt.Errorf("email is not provided")
 	}
@@ -94,5 +100,71 @@ func (s *Server) CreateAuthenticationCookie(ctx context.Context, session gocqlx.
 	cookie.Redeemed = authCookie.Redeemed
 	cookie.Expiration = authCookie.Expiration.String()
 
+	return &evav1.CreateAuthenticationCookieResponse{Cookie: cookie}, nil
+}
+
+func (s *Server) GetAuthenticationCookie(ctx context.Context, session gocqlx.Session, request *evav1.GetAuthenticationCookieRequest) (*evav1.GetAuthenticationCookieResponse, error) {
+	if request == nil || request.Cookie == "" {
+		return nil, fmt.Errorf("cookie is not provided")
+	}
+
+	u, err := gocql.ParseUUID(request.Cookie)
+
+	if err != nil {
+		return nil, fmt.Errorf("uuid is not valid")
+	}
+
+	queryCookie := qb.Select("authentication_cookies").Columns("cookie", "email", "redeemed", "expiration").Query(session)
+
+	// get authentication cookie with this ID
+	authCookie := AuthenticationCookie{
+		ID: u,
+	}
+
+	queryCookie.BindStruct(authCookie)
+
+	var cookieItem *AuthenticationCookie
+
+	if err := queryCookie.Select(&cookieItem); err != nil {
+		return nil, fmt.Errorf("select() failed: '%s", err)
+	}
+
+	cookie := new(evav1.AuthenticationCookie)
+	cookie.Email = cookieItem.Email
+	cookie.Redeemed = cookieItem.Redeemed
+	cookie.Expiration = cookieItem.Expiration.String()
+
 	return &evav1.GetAuthenticationCookieResponse{Cookie: cookie}, nil
+}
+
+func (s *Server) GetRegisteredEmail(ctx context.Context, session gocqlx.Session, request *evav1.GetRegisteredEmailRequest) (*evav1.GetRegisteredEmailResponse, error) {
+	if request == nil || request.Email == "" {
+		return nil, fmt.Errorf("email is not provided")
+	}
+
+	if !emailRegex.MatchString(request.Email) {
+		return nil, fmt.Errorf("email is not valid")
+	}
+
+	// Check for registered user with this email
+	resource := RegisteredUser{
+		Email: request.Email,
+	}
+
+	// check if email is in use
+	queryEmail := qb.
+		Select("users_emails").
+		Columns("email", "username").
+		Where(qb.Eq("email")).
+		Query(session).
+		BindStruct(resource)
+
+	var registeredItem *RegisteredUser
+
+	if err := queryEmail.Select(&registeredItem); err != nil {
+		return nil, fmt.Errorf("select() failed: '%s", err)
+	}
+
+	return &evav1.GetRegisteredEmailResponse{Username: registeredItem.Username}, nil
+
 }
