2c00308ac3130997a90b8505973399a4253e098f
nikita-tkachov <nikita.tkachov@protonmail.com>
2020-12-28 15:12:28 -0500

    add subscription

--- applications/hades/resolvers/subscription.resolvers.go
+++ applications/hades/resolvers/subscription.resolvers.go
@@ -5,15 +5,41 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
+	evav1 "project01101000/codebase/applications/eva/proto"
 	"project01101000/codebase/applications/hades/gen"
 	"project01101000/codebase/applications/hades/model"
+	"project01101000/codebase/applications/hades/src/helpers"
 )
 
 func (r *subscriptionResolver) AuthenticationState(ctx context.Context) (<-chan *model.AuthenticationState, error) {
 	// AuthenticationState - check the state of our authentication by checking the OTP Cookie header to see if we have redeemed it
+	out := make(chan *model.AuthenticationState)
 
-	panic(fmt.Errorf("not implemented"))
+	gc, err := helpers.GinContextFromContext(ctx)
+
+	if err != nil {
+		close(out)
+		return nil, err
+	}
+
+	currentCookie, err := gc.Request.Cookie("otp-cookie")
+
+	if err != nil || currentCookie == nil {
+		close(out)
+		return nil, err
+	}
+
+	// Get our cookie so we can get our email
+	getAuthenticationCookie, err := r.services.Eva().GetAuthenticationCookie(ctx, &evav1.GetAuthenticationCookieRequest{Cookie: currentCookie.Value})
+
+	if err != nil {
+		close(out)
+		return nil, err
+	}
+
+	out <- &model.AuthenticationState{Redeemed: getAuthenticationCookie.Cookie.Redeemed}
+
+	return out, nil
 }
 
 // Subscription returns gen.SubscriptionResolver implementation.
