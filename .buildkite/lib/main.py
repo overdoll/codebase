d4e21193aa3776df870d0c137d1bd22af1072ecb
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-14 17:24:56 -0400

    ls

--- .buildkite/lib/main.py
+++ .buildkite/lib/main.py
@@ -191,7 +191,7 @@ def print_project_pipeline():
     pipeline_steps.append(
         create_step(
             label=":test_tube: Integration Test",
-            commands=["pwd", ".buildkite/pipeline.sh integration_test"],
+            commands=["ls", ".buildkite/pipeline.sh integration_test"],
             platform="docker-compose",
             # Include docker-compose configs from all configurations, plus our custom one - the container in which the
             # integration tests will actually be ran
