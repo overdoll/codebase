92545f6ec4a823664f22d019c7fda287539cd851
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-14 16:50:32 -0400

    update docker config

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,7 +3,4 @@
 # exit immediately on failure, or if an undefined variable is used
 set -eu
 
-# need to copy docker config from instance to here, so it's accessible
-cp /var/lib/buildkite-agent/$DOCKER_CONFIG/config.json ~/.docker/config.json
-
 python3.6 ./.buildkite/lib/main.py "$@"
