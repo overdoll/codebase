e3ea8d22fc2c2760e592e52e2fbee5fd377f7280
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-14 23:20:54 -0400

    use build number

--- .buildkite/lib/main.py
+++ .buildkite/lib/main.py
@@ -148,7 +148,8 @@ def create_docker_compose_step(label, commands, additional_env_vars=None, config
         "BUILDKITE_JOB_ID",
         "BUILDKITE_BUILD_ID",
         "BUILDKITE_AGENT_ACCESS_TOKEN",
-        "CONTAINER_REGISTRY"
+        "CONTAINER_REGISTRY",
+        "BUILDKITE_BUILD_NUMBER"
     ]
 
     return {
@@ -202,7 +203,7 @@ def print_project_pipeline():
     pipeline_steps.append(
         create_step(
             label=":test_tube: Integration Test",
-            commands=["docker logs buildkite{}_scylla_1".format(os.getenv("BUILDKITE_BUILD_ID")).replace("-", ""),
+            commands=["docker logs buildkite{}_scylla_1".format(os.getenv("BUILDKITE_BUILD_NUMBER")).replace("-", ""),
                       ".buildkite/pipeline.sh integration_test"],
             platform="docker-compose",
             # Include docker-compose configs from all configurations, plus our custom one - the container in which the
