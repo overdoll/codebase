33dbcd5c02d9f3dad05cac70fc1096d238d472bf
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-15 19:57:36 -0400

    wait for it

--- .buildkite/lib/main.py
+++ .buildkite/lib/main.py
@@ -7,9 +7,11 @@ import glob
 import os
 import random
 import shutil
+import socket
 import sys
 import tempfile
 import threading
+import time
 
 import utils.bazel as bazel
 import utils.exception as exception
@@ -23,6 +25,27 @@ random.seed()
 
 class YamlReaderError(Exception):
     pass
+
+
+def wait_for_port(port, host='localhost', timeout=5.0):
+    """Wait until a port starts accepting TCP connections.
+    Args:
+        port (int): Port number.
+        host (str): Host address on which the port should exist.
+        timeout (float): In seconds. How long to wait before raising errors.
+    Raises:
+        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
+    """
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
+                                   'connections.'.format(port, host)) from ex
 
 
 def data_merge(a, b):
@@ -245,6 +268,9 @@ def print_project_pipeline():
 # execute commands to run integration tests
 def execute_integration_tests_commands(configs):
     tmpdir = tempfile.mkdtemp()
+
+    wait_for_port(8000, "sting", 30)
+    wait_for_port(8000, "eva", 30)
 
     try:
         test_env_vars = ["HOME"]
