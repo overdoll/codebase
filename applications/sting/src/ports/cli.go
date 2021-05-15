f15057a538b0bb7bf3e82efb907ddeebc0ceaf03
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-15 16:04:39 -0400

    update CLI to work correctly

--- applications/sting/src/ports/cli.go
+++ applications/sting/src/ports/cli.go
@@ -14,17 +14,17 @@ var Root = &cobra.Command{
 }
 
 func init() {
-	ctx, cancelFn := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancelFn()
-
-	application, cleanup := service.NewApplication(ctx)
-
-	defer cleanup()
-
 	Root.AddCommand(&cobra.Command{
 		Use:   "artists",
 		Short: "Index the whole artists table into elasticsearch",
 		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancelFn()
+
+			application, cleanup := service.NewApplication(ctx)
+
+			defer cleanup()
+
 			if err := application.Commands.IndexAllArtists.Handle(ctx); err != nil {
 				log.Fatalf(err.Error())
 			}
@@ -35,6 +35,13 @@ func init() {
 		Use:   "categories",
 		Short: "Index the whole categories table into elasticsearch",
 		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancelFn()
+
+			application, cleanup := service.NewApplication(ctx)
+
+			defer cleanup()
+
 			if err := application.Commands.IndexAllCategories.Handle(ctx); err != nil {
 				log.Fatalf(err.Error())
 			}
@@ -45,6 +52,13 @@ func init() {
 		Use:   "characters",
 		Short: "Index the whole characters table into elasticsearch",
 		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancelFn()
+
+			application, cleanup := service.NewApplication(ctx)
+
+			defer cleanup()
+
 			if err := application.Commands.IndexAllCharacters.Handle(ctx); err != nil {
 				log.Fatalf(err.Error())
 			}
@@ -55,6 +69,13 @@ func init() {
 		Use:   "media",
 		Short: "Index the whole media table into elasticsearch",
 		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancelFn()
+
+			application, cleanup := service.NewApplication(ctx)
+
+			defer cleanup()
+
 			if err := application.Commands.IndexAllMedia.Handle(ctx); err != nil {
 				log.Fatalf(err.Error())
 			}
