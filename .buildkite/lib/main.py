21258d184ebbf8ee7579d2c661c7f32d30b19eda
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-14 23:28:53 -0400

    try another

--- .buildkite/lib/main.py
+++ .buildkite/lib/main.py
@@ -203,7 +203,7 @@ def print_project_pipeline():
     pipeline_steps.append(
         create_step(
             label=":test_tube: Integration Test",
-            commands=["docker logs buildkite{}_scylla_1".format(os.getenv("BUILDKITE_BUILD_NUMBER")).replace("-", ""),
+            commands=["docker logs buildkite{}_scylla_1".format(os.getenv("BUILDKITE_JOB_ID")).replace("-", ""),
                       ".buildkite/pipeline.sh integration_test"],
             platform="docker-compose",
             # Include docker-compose configs from all configurations, plus our custom one - the container in which the
