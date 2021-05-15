40dd63893ef358a11f5b2e3651433343f8afdc39
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-15 00:03:57 -0400

    fix again?

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,7 +5,7 @@ set -eu
 
 if [ "$1" == "integration_test" ]
   then
-    docker logs $(docker ps | grep \"scylladb/scylla:4.3.1\" | awk '{ print $1 }')
+    docker logs "$(docker ps | grep "scylladb/scylla:4.3.1" | awk '{ print $1 }')"
 fi
 
 python3 ./.buildkite/lib/main.py "$@"
