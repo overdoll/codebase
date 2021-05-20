5ae787ee2dcc6e9ec04ab7e2870a1b3f3c017cbf
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-19 22:27:20 -0400

    update

--- applications/medusa/src/client/components/routes/token/__tests__/Token.ispec.js
+++ applications/medusa/src/client/components/routes/token/__tests__/Token.ispec.js
@@ -1,47 +1,47 @@
-import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils';
-import withProviders from '@//:modules/testing/withProviders';
-import Token from '../Token';
-import { render, screen } from '@testing-library/react';
-import { loadQuery } from 'react-relay/hooks';
+import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils'
+import withProviders from '@//:modules/testing/withProviders'
+import Token from '../Token'
+import { render, screen } from '@testing-library/react'
+import { loadQuery } from 'react-relay/hooks'
 
 const queueOperation = resolver => {
-  const Environment = createMockEnvironment();
+  const Environment = createMockEnvironment()
 
-  const TokenQuery = require('@//:artifacts/TokenQuery.graphql');
+  const TokenQuery = require('@//:artifacts/TokenQuery.graphql')
 
-  const cookie = 'fake-token-123';
+  const cookie = 'fake-token-123'
 
   Environment.mock.queuePendingOperation(TokenQuery, {
-    cookie: cookie,
-  });
+    cookie: cookie
+  })
 
   // Set query to return our "fake" data
   Environment.mock.queueOperationResolver(operation =>
-    MockPayloadGenerator.generate(operation, resolver),
-  );
+    MockPayloadGenerator.generate(operation, resolver)
+  )
 
   const preparedQuery = loadQuery(
     Environment,
     TokenQuery,
     {
-      cookie: cookie,
+      cookie: cookie
     },
     {
-      fetchPolicy: 'store-or-network',
-    },
-  );
+      fetchPolicy: 'store-or-network'
+    }
+  )
 
   // Pass preload query data to token, since it originally resolves this from the router
   const TokenComponent = () => {
-    return <Token prepared={{ tokenQuery: preparedQuery }} />;
-  };
+    return <Token prepared={{ tokenQuery: preparedQuery }} />
+  }
 
   return withProviders({
     Component: TokenComponent,
     routes: [],
-    environment: Environment,
-  });
-};
+    environment: Environment
+  })
+}
 
 it('should ask to register if not registered', async () => {
   const resolver = {
@@ -49,18 +49,18 @@ it('should ask to register if not registered', async () => {
       sameSession: true,
       registered: false,
       invalid: false,
-      session: '',
-    }),
-  };
+      session: ''
+    })
+  }
 
-  const [Root] = queueOperation(resolver);
+  const [Root] = queueOperation(resolver)
 
-  render(<Root />);
+  render(<Root />)
 
   // If there is a button, we are at a registration screen
-  expect(screen.getByRole('textbox')).toBeVisible();
-  expect(screen.getByRole('button')).toBeVisible();
-});
+  expect(screen.getByRole('textbox')).toBeVisible()
+  expect(screen.getByRole('button')).toBeVisible()
+})
 
 //   // TODO: fix this test
 // it('should redirect if cookie is not valid', async () => {
@@ -83,17 +83,17 @@ it('should show session data if token was redeemed in another session', async ()
       sameSession: false,
       registered: false,
       invalid: false,
-      session: '{ "user-agent": "agent-session" }',
-    }),
-  };
+      session: '{ "user-agent": "agent-session" }'
+    })
+  }
 
-  const [Root] = queueOperation(resolver);
+  const [Root] = queueOperation(resolver)
 
-  render(<Root />);
+  render(<Root />)
 
   // Expect session data to be visible
-  expect(screen.getByText('agent-session')).toBeVisible();
-});
+  expect(screen.getByText('agent-session')).toBeVisible()
+})
 
 it('should redirect if user is already registered', async () => {
   const resolver = {
@@ -101,14 +101,14 @@ it('should redirect if user is already registered', async () => {
       sameSession: true,
       registered: true,
       invalid: false,
-      session: '{ "user-agent": "agent-session" }',
-    }),
-  };
+      session: '{ "user-agent": "agent-session" }'
+    })
+  }
 
-  const [Root, router] = queueOperation(resolver);
+  const [Root, router] = queueOperation(resolver)
 
-  render(<Root />);
+  render(<Root />)
 
   // user was redirected to '/profile'
-  expect(router.context.history.location.pathname).toEqual('/profile');
-});
+  expect(router.context.history.location.pathname).toEqual('/profile')
+})
--- relay.config.js
+++ relay.config.js
@@ -1,16 +1,13 @@
-const { resolve } = require('path');
+const { resolve } = require('path')
 
 module.exports = {
   schema: resolve(__dirname, 'applications/medusa/schema/schema.graphql'),
   src: resolve(__dirname, 'applications/medusa/src'),
   persistOutput: resolve(
     __dirname,
-    'applications/medusa/src/server/queries.json',
+    'applications/medusa/src/server/queries.json'
   ),
   watch: true,
   language: 'js-flow-uncommented',
-  artifactDirectory: resolve(
-    __dirname,
-    'applications/medusa/src/__generated__',
-  ),
-};
+  artifactDirectory: resolve(__dirname, 'applications/medusa/src/__generated__')
+}
