f1a84c4c9a8cd1cde933f737386ca9224e98cf7b
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-27 15:32:13 -0400

    update check now does not do any route changes except refreshing

--- applications/medusa/src/client/utilities/update/index.js
+++ applications/medusa/src/client/utilities/update/index.js
@@ -1,36 +1,27 @@
 /**
  * @flow
  */
-import type { RouterInstance } from '@//:modules/routing/router'
+import type { Location, RouterInstance } from '@//:modules/routing/router'
 import BatteryFriendlyTimer from '@//:modules/timer'
 import { getEnv } from '@//:modules/runtime'
 import axios from 'axios'
 
 const timer = new BatteryFriendlyTimer(window)
 
-let UpdateIsScheduled = false
 let PendingRequest = false
 
-export function registerUpdateListener (router: RouterInstance): boolean {
+export function registerUpdateListener (router: RouterInstance) {
   timer.setInterval(
-    checkForUpdate,
-    10 * 60 * 1000, // check every 10 minutes if network is available
+    () => checkForUpdate(router),
+    10, // check every 10 minutes if network is available
     24 * 60 * 60 * 1000 // check every day even if no network activity
   )
 
   // Subscribe to router change - only update our timer everytime the user's location
   // is changed, since this indicates "activity"
-  // If an update is scheduled for this route change, then perform it
   router.context.history.listen(() => {
-    if (UpdateIsScheduled) {
-      router.context.history.go(0)
-      return
-    }
-
     timer.fetchHappens()
   })
-
-  return UpdateIsScheduled
 }
 
 // Export a function to refresh the timer so we can use it in other places,
@@ -39,20 +30,34 @@ export async function refreshUpdateTimer () {
   await timer.fetchHappens()
 }
 
+// PerformUpdate is a simple function that will perform an update
+// It just sets the location to where the user was originally supposed to go
+// Return false makes it so that history won't do it's usual navigation
+function performUpdate (location: Location) {
+  window.location.href = `${location.pathname}${location.search}`
+  return false
+}
+
 // Check for update
-// If an update is needed, the location will reload, as the route changes
-function checkForUpdate () {
-  if (PendingRequest || UpdateIsScheduled) return
+// If an update is needed, the location will reload
+function checkForUpdate (router: RouterInstance) {
+  if (PendingRequest) return
 
   PendingRequest = true
 
   // fetch version
   axios.get('/api/version')
     .then(response => {
-      PendingRequest = false
       // no version change, proceed
-      if (getEnv('APP_VERSION') === response.data) return
-      // On the next route change, the page will reload
-      UpdateIsScheduled = true
+      // unblock with PendingRequest as well
+      if (getEnv('APP_VERSION') !== response.data) {
+        // Block the history
+        // When blocked, the function in the callback will be called next time a route
+        // change is supposed to happen
+        router.context.history.block(performUpdate)
+        return
+      }
+
+      PendingRequest = false
     })
 }
