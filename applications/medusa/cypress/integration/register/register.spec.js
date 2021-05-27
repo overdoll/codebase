6174af831c73373c6e4ef17b68b1a83231678ee7
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-27 14:00:33 -0400

    fix test by waiting for email to show before checking for cookie

--- applications/medusa/cypress/integration/register/register.spec.js
+++ applications/medusa/cypress/integration/register/register.spec.js
@@ -2,23 +2,9 @@ describe('Register', () => {
   const id = Cypress._.random(0, 1e6)
 
   beforeEach(() => {
-    cy.visit('/join')
-
     const email = id + '@test.com'
 
-    cy.get('form')
-      .findByRole('textbox', { name: /email/iu })
-      .type(email)
-
-    cy.get('form')
-      .findByRole('button', { name: /Continue/iu })
-      .click()
-
-    cy.getCookie('otp-key').then(cookie => {
-      // in debug mode, our cookies won't be encrypted so we can just read it directly from the browser
-      // in production, the user would have to check their email in order to get the right token
-      cy.visit('/token/' + cookie.value)
-    })
+    cy.login(email)
   })
 
   it('should be able to register', () => {
