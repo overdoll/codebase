18f8644ef4fc978a171cff56f5368df382feff70
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-31 12:28:50 -0400

    show fallback if src is not a string

--- applications/medusa/src/modules/utilities/SuspenseImage.js
+++ applications/medusa/src/modules/utilities/SuspenseImage.js
@@ -15,34 +15,37 @@ type Props = {
 function SuspenseImage (props: Props): Node {
   const { fallback, src, ...rest } = props
 
+  // if SRC is undefined or null, we will just show a fallback
+  if (typeof src !== 'string') {
+    return fallback
+  }
+
   // On the server, we use a regular img
   if (!CanUseDOM) {
     return <Img src={src} {...rest} />
   }
 
-  if (src !== null) {
-    // JSResource is meant for loading resources, but the implementation is
-    // just cached loading of promises. So we reuse that here as a quick
-    // way to suspend while images are loading, with caching in case
-    // we encouter the same image twice (in that case, we'll create
-    // new loader *functions*, but JSResource will return a cached
-    // value and only load the image once.
-    const resource = JSResource(src, () => {
-      return new Promise(resolve => {
-        const img = new window.Image()
-        img.onload = () => {
-          resolve(src)
-        }
-        img.onerror = () => {
-          resolve(src)
-        }
-        img.src = src
-      })
+  // JSResource is meant for loading resources, but the implementation is
+  // just cached loading of promises. So we reuse that here as a quick
+  // way to suspend while images are loading, with caching in case
+  // we encouter the same image twice (in that case, we'll create
+  // new loader *functions*, but JSResource will return a cached
+  // value and only load the image once.
+  const resource = JSResource(src, () => {
+    return new Promise(resolve => {
+      const img = new window.Image()
+      img.onload = () => {
+        resolve(src)
+      }
+      img.onerror = () => {
+        resolve(src)
+      }
+      img.src = src
     })
+  })
 
-    resource.load()
-    resource.read() // suspends while the image is pending
-  }
+  resource.load()
+  resource.read() // suspends while the image is pending
 
   return <Image src={src} ignoreFallback {...rest} />
 }
