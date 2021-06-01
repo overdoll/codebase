c8333dc13edc102d80457b9f77d7539b2c910862
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-06-01 15:26:26 -0400

    fix import

--- applications/medusa/src/client/domain/Join/Lobby/__tests__/Lobby.ispec.js
+++ applications/medusa/src/client/domain/Join/Lobby/__tests__/Lobby.ispec.js
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils'
 import withProviders from '@//:modules/testing/withProviders'
-import Index from '../Lobby'
+import Lobby from '../'
 
 it('should run the parent function when subscription completes with desired results', async () => {
   const Environment = createMockEnvironment()
@@ -11,7 +11,7 @@ it('should run the parent function when subscription completes with desired resu
   const email = 'test@test.com'
 
   const LobbyComponent = () => (
-    <Index email={email} onReceive={onReceiveMock} />
+    <Lobby email={email} onReceive={onReceiveMock} />
   )
 
   const [Root] = withProviders({
