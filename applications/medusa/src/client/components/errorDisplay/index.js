756d35787582f5f3a22fcc927f2222e145d8f614
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-05-31 21:46:49 -0400

    fix props

--- applications/medusa/src/client/components/errorDisplay/index.js
+++ applications/medusa/src/client/components/errorDisplay/index.js
@@ -1,10 +1,18 @@
+/**
+ * @flow
+ */
 import { Center, Code, Flex } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet-async'
+import type { Node } from 'react'
+
+type Props = {
+  error: Error
+}
 
 // This component displays an error page when an error occurs on the server
 // TODO: make this page look nice, since users may sometimes see this during downtime or crashes
 // TODO: also need to report the error to sentry
-export default function ErrorDisplay ({ error }) {
+export default function ErrorDisplay ({ error }: Props): Node {
   const debug = process.env.APP_DEBUG === 'true'
 
   if (debug) console.log(error)
