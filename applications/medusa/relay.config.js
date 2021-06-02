745d08d571577bb0e4a2852bda3ebe81a0e9d6f3
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-06-01 21:23:40 -0400

    update

---
+++ applications/medusa/relay.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path')
+
+module.exports = {
+  schema: resolve(__dirname, 'schema/schema.graphql'),
+  src: resolve(__dirname, 'src'),
+  persistOutput: resolve(
+    __dirname,
+    'src/server/queries.json'
+  ),
+  watch: true,
+  language: 'js-flow-uncommented',
+  artifactDirectory: resolve(
+    __dirname,
+    'src/__generated__'
+  )
+}
