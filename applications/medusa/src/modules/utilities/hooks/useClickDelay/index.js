6eb264df2854b80131af9c1435d996058b3f234a
Ilya <ilya.tkachov@hotmail.com>
2021-06-05 17:24:02 -0400

    useClickDelay implementation

---
+++ applications/medusa/src/modules/utilities/hooks/useClickDelay/index.js
@@ -0,0 +1,53 @@
+/**
+ * @flow
+ */
+
+import { useState, useEffect } from 'react'
+
+export default function useClickDelay (storageVariableName = 'hasClickDelay') {
+  const [currentTimer, setTimer] = useState(0)
+  const [isTimedOut, setTimedOut] = useState(false)
+  const [localVariable, setLocalVariable] = useState('empty')
+  const [checked, setChecked] = useState(false)
+
+  useEffect(() => {
+    setLocalVariable(sessionStorage.getItem(storageVariableName))
+    if (localVariable !== 'empty' && !checked) {
+      if (localVariable != null) {
+        setTimer(parseInt(localVariable))
+        createTimer(parseInt(localVariable))
+        setTimedOut(true)
+      }
+      setChecked(true)
+    }
+  })
+
+  const createTimer = (time: number) => {
+    const interval = setInterval(() => {
+      setTimer(x => {
+        sessionStorage.setItem(storageVariableName, x - 1000)
+        return x - 1000
+      })
+    }, 1000)
+
+    setTimeout(() => {
+      clearTimeout(interval)
+    }, time)
+  }
+
+  const createTimeout = (timeOutLength: number) => {
+    if (!localVariable) {
+      setTimer(timeOutLength)
+      createTimer(timeOutLength)
+      setTimedOut(true)
+    }
+  }
+
+  const clearTimeout = interval => {
+    setTimedOut(false)
+    clearInterval(interval)
+    sessionStorage.removeItem(storageVariableName)
+  }
+
+  return [isTimedOut, currentTimer, createTimeout]
+}
