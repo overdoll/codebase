b929f0a41bd21020a2cf59ece56fffeaca027a94
nikita-tkachov <nikita.tkachov@protonmail.com>
2021-06-16 11:35:33 -0400

    add service

--- applications/medusa/src/server/config/services.js
+++ applications/medusa/src/server/config/services.js
@@ -1,4 +1,5 @@
 export default [
   { name: 'eva', url: 'http://eva:8000/graphql' },
-  { name: 'sting', url: 'http://sting:8000/graphql' }
+  { name: 'sting', url: 'http://sting:8000/graphql' },
+  { name: 'parley', url: 'http://parley:8000/graphql' }
 ]
