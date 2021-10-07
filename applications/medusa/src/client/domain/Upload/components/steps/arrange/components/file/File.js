59c7d42490c2fb08bf6283bfef5a6498d9e68406
Ilya <ilya.tkachov@hotmail.com>
2021-10-07 16:48:05 -0400

    fix for broken link

--- applications/medusa/src/client/domain/Upload/components/steps/arrange/components/file/File.js
+++ applications/medusa/src/client/domain/Upload/components/steps/arrange/components/file/File.js
@@ -7,8 +7,6 @@ import { Flex, Heading, Text, Skeleton, CloseButton } from '@chakra-ui/react'
 import Thumbnail from '../thumbnail/Thumbnail'
 import Icon from '@//:modules/content/Icon/Icon'
 
-import InterfacePageControllerScrollUp
-  from '@streamlinehq/streamlinehq/img/streamline-mini-bold/interface-essential/page-controller/interface-page-controller-scroll-up.svg'
 import InterfaceArrowsButtonDown
   from '@streamlinehq/streamlinehq/img/streamline-mini-bold/interface-essential/arrows/interface-arrows-button-down.svg'
 
@@ -48,7 +46,6 @@ export default function File ({
           {...provided.dragHandleProps}
         >
           <Flex w='8%' justifyContent='center' direction='column' alignItems='center'>
-            <Icon mb={3} icon={InterfacePageControllerScrollUp} fill='gray.600' h={5} w={5} />
             <Heading fontSize='lg' color='gray.100'>
               {index + 1}
             </Heading>
