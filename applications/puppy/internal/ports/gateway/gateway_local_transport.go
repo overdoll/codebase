68e47105ba00c76b3aa15f45ce0af16d5d31a49e
Nikita <nikita.tkachov@protonmail.com>
2021-12-04 20:24:50 -0500

    more changes

--- applications/puppy/internal/ports/gateway/gateway_local_transport.go
+++ applications/puppy/internal/ports/gateway/gateway_local_transport.go
@@ -2,21 +2,47 @@ package gateway
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 )
+
+var structure struct {
+	data struct {
+		node struct {
+			__typename string
+			id         string
+		}
+	}
+}
 
 type gatewayLocalTransport struct{}
 
 func (h *gatewayLocalTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 
-	//fmt.Println(req.Body.Read())
+	var d map[string]interface{}
 
-	fmt.Println("run")
+	dec := json.NewDecoder(req.Body)
+
+	if err := dec.Decode(&d); err != nil {
+		return nil, err
+	}
+
+	// get variable ID, that's all we need
+	// variableId := d["variables"].(map[string]interface{})["a"].(string)
+
+	body := `{ "data": { "node": { "__typename": "Account", "id": "12" } } }`
 
 	t := &http.Response{
-		Body: ioutil.NopCloser(bytes.NewBufferString(`{ "data": { "account": { "id": 12 } } }`)),
+		Body:          ioutil.NopCloser(bytes.NewBufferString(body)),
+		Status:        "200 OK",
+		StatusCode:    200,
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(body)),
+		Request:       req,
+		Header:        make(http.Header, 0),
 	}
 
 	buff := bytes.NewBuffer(nil)
