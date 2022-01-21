d9517d3a884e58609b2ba16eca6a8b9ef50e75af
Nikita <nikita.tkachov@protonmail.com>
2022-01-20 20:47:59 -0500

    update hosts

--- libraries/bootstrap/database.go
+++ libraries/bootstrap/database.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -13,7 +14,7 @@ import (
 func initializeDatabaseSession(keyspace string) (gocqlx.Session, error) {
 
 	// Create gocql cluster
-	cluster := gocql.NewCluster(os.Getenv("DB_HOST"))
+	cluster := gocql.NewCluster(strings.Split(os.Getenv("DB_HOST"), ",")...)
 
 	if keyspace != "" {
 		cluster.Keyspace = keyspace
