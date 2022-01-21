676cab4c7b221fe697aad2f6b800619d1344f24d
Nikita <nikita.tkachov@protonmail.com>
2022-01-20 23:36:46 -0500

    log config

--- applications/medusa/src/server/app/render/index.tsx
+++ applications/medusa/src/server/app/render/index.tsx
@@ -195,8 +195,6 @@ async function request (req, res): Promise<void> {
     renderToString(extractor.collectChunks(App))
   )
 
-  console.log(extractor.getLinkTags())
-
   res.render('index', {
     title: helmetContext?.helmet?.title.toString(),
     meta: helmetContext?.helmet?.meta.toString(),
