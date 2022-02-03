66621415460d22ff22aebbffdf6d1ce56ca1035a
Nikita <nikita.tkachov@protonmail.com>
2022-02-02 22:26:22 -0500

    apparently need this lol

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,4 +3,9 @@
 # exit immediately on failure, or if an undefined variable is used
 set -eu
 
+# a hack to install the yaml package, if it's not available
+if ! python -c "import yaml" &>/dev/null; then
+  apt install python3-yaml
+fi
+
 python3 ./.buildkite/lib/main.py "$@"
