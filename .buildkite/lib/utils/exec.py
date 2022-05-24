061cdf131eaeb1fe421891f7b93d55ecc3378341
Nikita <nikita.tkachov@protonmail.com>
2022-05-24 12:47:17 -0400

    fix again maybe

--- .buildkite/lib/utils/exec.py
+++ .buildkite/lib/utils/exec.py
@@ -8,14 +8,14 @@ def execute_command(args, shell=False, fail_if_nonzero=True, cwd=None, print_out
     if print_output:
         terminal_print.eprint(" ".join(args))
     return subprocess.run(
-        ['/bin/bash', '-i', '-c', " ".join(args)], shell=shell, check=fail_if_nonzero, env=os.environ, cwd=cwd,
+        ['/bin/bash', '-e', '-c', " ".join(args)], shell=shell, check=fail_if_nonzero, env=os.environ, cwd=cwd,
     ).returncode
 
 
 def execute_command_and_get_output(args, shell=False, fail_if_nonzero=True, print_output=True):
     terminal_print.eprint(" ".join(args))
     process = subprocess.run(
-        ['/bin/bash', '-i', '-c', " ".join(args)],
+        ['/bin/bash', '-e', '-c', " ".join(args)],
         shell=shell,
         check=fail_if_nonzero,
         env=os.environ,
