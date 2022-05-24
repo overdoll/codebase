775a71b5373a7155df43479381034aebd067cf73
Nikita <nikita.tkachov@protonmail.com>
2022-05-24 12:08:36 -0400

    shell back to false, add bash flag

--- .buildkite/lib/utils/exec.py
+++ .buildkite/lib/utils/exec.py
@@ -4,18 +4,18 @@ import subprocess
 from . import terminal_print
 
 
-def execute_command(args, shell=True, fail_if_nonzero=True, cwd=None, print_output=True):
+def execute_command(args, shell=False, fail_if_nonzero=True, cwd=None, print_output=True):
     if print_output:
         terminal_print.eprint(" ".join(args))
     return subprocess.run(
-        ['/bin/bash', '-c', " ".join(args)], shell=shell, check=fail_if_nonzero, env=os.environ, cwd=cwd,
+        ['/bin/bash', '-i', '-c', " ".join(args)], shell=shell, check=fail_if_nonzero, env=os.environ, cwd=cwd,
     ).returncode
 
 
-def execute_command_and_get_output(args, shell=True, fail_if_nonzero=True, print_output=True):
+def execute_command_and_get_output(args, shell=False, fail_if_nonzero=True, print_output=True):
     terminal_print.eprint(" ".join(args))
     process = subprocess.run(
-        ['/bin/bash', '-c', " ".join(args)],
+        ['/bin/bash', '-i', '-c', " ".join(args)],
         shell=shell,
         check=fail_if_nonzero,
         env=os.environ,
