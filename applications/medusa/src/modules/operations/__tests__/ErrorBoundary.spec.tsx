3fffa208985b0deab3da58d65bd501e11815a07e
Ilya <ilya.tkachov@hotmail.com>
2022-06-18 19:58:30 -0400

    test fix

--- applications/medusa/src/modules/operations/__tests__/ErrorBoundary.spec.tsx
+++ applications/medusa/src/modules/operations/__tests__/ErrorBoundary.spec.tsx
@@ -63,7 +63,7 @@ it('should catch error when thrown', async () => {
   render(<Root />)
 
   // error thrown by our component
-  await waitFor(() => expect(screen.getByText('An error has occurred')).toBeVisible())
+  await waitFor(() => expect(screen.getByText('Error')).toBeVisible())
 })
 
 it('should render fallback when error is thrown', async () => {
