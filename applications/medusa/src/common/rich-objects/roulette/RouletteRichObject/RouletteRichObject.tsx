490416fbc7568ff8ce6277af090d30a3239a2185
ilyat <ilya.tkachov@hotmail.com>
2022-09-02 13:25:35 -0400

    fixes (#304)

--- applications/medusa/src/common/rich-objects/roulette/RouletteRichObject/RouletteRichObject.tsx
+++ applications/medusa/src/common/rich-objects/roulette/RouletteRichObject/RouletteRichObject.tsx
@@ -5,7 +5,7 @@ import { TITLE_FEATURES } from '@//:modules/constants/rich-objects'
 export default function RouletteRichObject (): JSX.Element {
   const TITLE = `Rule34 Roulette - Spin your way through ${TITLE_FEATURES}`
 
-  const DESCRIPTION = 'Play the Rule34 Roulette on overdoll and see how long you can last before you have to lose!'
+  const DESCRIPTION = 'Play the Rule34 Roulette on overdoll and see how long you can last before you HAVE to lose!'
 
   const URL = 'https://overdoll.com/roulette'
 
@@ -36,6 +36,7 @@ export default function RouletteRichObject (): JSX.Element {
         <meta property='og:image:type' content='image/jpeg' />
         <meta property='og:image:width' content='1827' />
         <meta property='og:image:height' content='1145' />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
     </>
   )
