94af1eb4f2726a7abcff526aeaf2ab5bfaa1dcb9
ilyat <ilya.tkachov@hotmail.com>
2022-09-13 00:34:24 -0400

    tus changes to maybe fix upload timing out on 300mb uploads (#317)

--- applications/medusa/src/modules/content/HookedComponents/Upload/constants/upload.ts
+++ applications/medusa/src/modules/content/HookedComponents/Upload/constants/upload.ts
@@ -14,10 +14,9 @@ export const FILE_ICONS = {
   'video/webm': FileWebm
 }
 
-// TODO uppy doesn't expose the shouldTerminate variable to tus - custom plugin configuration is needed to pass in the parameter
 export const TUS_OPTIONS = {
   endpoint: '/api/upload/',
-  retryDelays: [0, 1000, 3000, 5000],
-  chunkSize: 104857600,
+  retryDelays: [0, 1000, 3000, 5000, 10000],
+  chunkSize: 1048576,
   shouldTerminate: false
 }
