2840b684b49531a5b19a4ed20df1a7e25eaf090f
Nikita <nikita.tkachov@protonmail.com>
2022-09-15 15:21:33 -0400

    Working on new serializer

--- applications/loader/internal/domain/media_processing/process.go
+++ applications/loader/internal/domain/media_processing/process.go
@@ -781,7 +781,8 @@ func processImage(media *media.Media, mimeType string, file *os.File) (*ProcessR
 			return nil, errors.Wrap(err, "failed to encode jpeg")
 		}
 	} else {
-		// if the source is a JPEG source, we just copy the file over and we don't make any changes
+		_, _ = file.Seek(0, io.SeekStart)
+		// if the source is a JPEG source, we just copy the file over, and we don't make any changes
 		imageFile, err = os.Create(fileName)
 		if err != nil {
 			return nil, err
