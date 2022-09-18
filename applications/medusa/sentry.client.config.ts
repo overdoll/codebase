7a26ae1220e29f8f0b5cf8799666ec39d9d7c4eb
Nikita <nikita.tkachov@protonmail.com>
2022-09-17 20:40:50 -0400

    Increase traces sample rate

--- applications/medusa/sentry.client.config.ts
+++ applications/medusa/sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 0.2,
+  tracesSampleRate: 1,
   release: process.env.NEXT_PUBLIC_APP_VERSION,
   environment: process.env.NEXT_PUBLIC_APP_ENV,
   beforeSend (event) {
