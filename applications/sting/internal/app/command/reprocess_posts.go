9bf3e452aaa0d5af878ec570eb673f485d9cb5bf
Nikita <nikita.tkachov@protonmail.com>
2022-10-02 22:13:13 -0400

    New reprocess individual

--- applications/sting/internal/app/command/reprocess_posts.go
+++ applications/sting/internal/app/command/reprocess_posts.go
@@ -22,26 +22,44 @@ func NewReprocessPostsMediaHandler(pr post.Repository, loader LoaderService) Rep
 }
 
 func (h ReprocessPostsMediaHandler) Handle(ctx context.Context, cmd ReprocessPostsMedia) error {
+
+	if cmd.PostId != "" {
+		pst, err := h.pr.GetPostByIdOperator(ctx, cmd.PostId)
+		if err != nil {
+			return err
+		}
+		var content []*media.Media
+		for _, pst := range pst.Content() {
+			content = append(content, pst.Media())
+			if pst.MediaHidden() != nil {
+				content = append(content, pst.MediaHidden())
+			}
+		}
+
+		if len(content) == 0 {
+			return nil
+		}
+
+		if err := h.loader.ReprocessMedia(ctx, content); err != nil {
+			return nil
+		}
+
+		zap.S().Infow("reprocessed post", zap.String("id", cmd.PostId))
+	}
+
+	return nil
+
 	return h.pr.ScanPosts(ctx, cmd.ClubId, cmd.PostId, func(post *post.Post) error {
 
 		var content []*media.Media
 
 		for _, pst := range post.Content() {
-
-			if cmd.PostId != "" {
+			if !pst.Media().IsLegacy() && pst.Media().HasAudio() {
 				content = append(content, pst.Media())
-				if pst.MediaHidden() != nil {
-					content = append(content, pst.MediaHidden())
-				}
-			} else {
-				if !pst.Media().IsLegacy() && pst.Media().HasAudio() {
-					content = append(content, pst.Media())
-				}
-				if pst.MediaHidden() != nil && !pst.MediaHidden().IsLegacy() && pst.Media().HasAudio() {
-					content = append(content, pst.MediaHidden())
-				}
 			}
-
+			if pst.MediaHidden() != nil && !pst.MediaHidden().IsLegacy() && pst.Media().HasAudio() {
+				content = append(content, pst.MediaHidden())
+			}
 		}
 
 		if len(content) == 0 {
