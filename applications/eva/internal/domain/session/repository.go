71d599500a39182514699837981b3cb6a9195650
Nikita <nikita.tkachov@protonmail.com>
2022-10-04 17:07:48 -0400

    Fix repository

--- applications/eva/internal/domain/session/repository.go
+++ applications/eva/internal/domain/session/repository.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"context"
+	"overdoll/libraries/paging"
 	"overdoll/libraries/passport"
 
 	"overdoll/libraries/principal"
@@ -9,6 +10,7 @@ import (
 
 type Repository interface {
 	GetLastActiveSessionByAccountIdOperator(ctx context.Context, accountId string) (*Session, error)
+	GetSessionsByAccountId(ctx context.Context, requester *principal.Principal, passport *passport.Passport, cursor *paging.Cursor, accountId string) ([]*Session, error)
 	GetSessionsByAccountIdOperator(ctx context.Context, accountId string) ([]*Session, error)
 	RevokeSessionById(ctx context.Context, requester *principal.Principal, passport *passport.Passport, sessionId string) error
 	GetSessionById(ctx context.Context, requester *principal.Principal, passport *passport.Passport, sessionId string) (*Session, error)
