cb095c929a7ac3ff4d0cc5635392e52c57f2f56b
Nikita <nikita.tkachov@protonmail.com>
2022-10-10 12:12:43 -0400

    Fix banner, skip e2e

--- applications/medusa/src/common/rich-objects/artists/ArtistsRichObject/CreatorsRichObject.tsx
+++ applications/medusa/src/common/rich-objects/artists/ArtistsRichObject/CreatorsRichObject.tsx
@@ -25,8 +25,8 @@ export default function CreatorsRichObject (): JSX.Element {
         />
         <meta name='twitter:card' content='summary_large_image' />
         <meta property='og:type' content='image' />
-        <meta property='og:image' content='/banner.jpg' />
-        <meta property='og:image:secure_url' content='/banner.jpg' />
+        <meta property='og:image' content='https://static.dollycdn.net/creators/banners/large.jpg' />
+        <meta property='og:image:secure_url' content='https://static.dollycdn.net/creators/banners/large.jpg' />
         <meta property='og:image:type' content='image/jpeg' />
         <meta property='og:image:width' content='2048px' />
         <meta property='og:image:height' content='1086px' />
