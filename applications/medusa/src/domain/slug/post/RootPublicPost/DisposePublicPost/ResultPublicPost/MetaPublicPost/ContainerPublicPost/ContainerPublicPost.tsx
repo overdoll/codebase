a42821f9992fb0fa66baca336f52731ab7c0a710
ilyat <ilya.tkachov@hotmail.com>
2022-10-10 13:48:57 -0400

    Ilya/branch84 (#340)
    * experiment fix
    
    * eslint fix
    
    skip e2e

--- applications/medusa/src/domain/slug/post/RootPublicPost/DisposePublicPost/ResultPublicPost/MetaPublicPost/ContainerPublicPost/ContainerPublicPost.tsx
+++ applications/medusa/src/domain/slug/post/RootPublicPost/DisposePublicPost/ResultPublicPost/MetaPublicPost/ContainerPublicPost/ContainerPublicPost.tsx
@@ -1,7 +1,7 @@
 import { graphql, useFragment } from 'react-relay/hooks'
 import type { ContainerPublicPostFragment$key } from '@//:artifacts/ContainerPublicPostFragment.graphql'
 import type { ContainerPublicPostViewerFragment$key } from '@//:artifacts/ContainerPublicPostViewerFragment.graphql'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import BannerPublicPost from './BannerPublicPost/BannerPublicPost'
 import DescriptionPublicPost from './DescriptionPublicPost/DescriptionPublicPost'
 import { BannerContainer, CinematicContainer, ContentContainer } from '@//:modules/content/PageLayout'
@@ -13,7 +13,7 @@ import { MagicWand } from '@//:assets/icons'
 import HomeRedirectPrompt from '@//:common/components/HomeRedirectPrompt/HomeRedirectPrompt'
 import PrepareGridSuggestedPosts from './PrepareGridSuggestedPosts/PrepareGridSuggestedPosts'
 import PrepareSuggestedPosts from './PrepareSuggestedPosts/PrepareSuggestedPosts'
-import posthog from 'posthog-js'
+import useFeatureFlag from '@//:modules/hooks/useFeatureFlag'
 
 interface Props {
   postQuery: ContainerPublicPostFragment$key
@@ -49,33 +49,27 @@ export default function ContainerPublicPost (props: Props): JSX.Element {
   const postData = useFragment(PostFragment, postQuery)
   const viewerData = useFragment(ViewerFragment, viewerQuery)
 
-  const [flag, setFlag] = useState(false)
+  const flag = useFeatureFlag('post-grid')
 
   const memoSuggestedPosts = useMemo(() => {
-    if (!flag) {
+    if (flag == null) {
       return (
         <PrepareSuggestedPosts postQuery={postData} />
       )
     }
 
-    if (posthog?.getFeatureFlag('join-modal') === 'control') {
+    if (flag === 'control') {
       return (
         <PrepareSuggestedPosts postQuery={postData} />
       )
     }
 
-    if (posthog?.getFeatureFlag('join-modal') === 'grid') {
+    if (flag === 'grid') {
       return <PrepareGridSuggestedPosts postQuery={postData} />
     }
 
     return <PrepareSuggestedPosts postQuery={postData} />
   }, [flag, postData.reference])
-
-  useEffect(() => {
-    posthog?.onFeatureFlags(() => {
-      setFlag(true)
-    })
-  }, [])
 
   return (
     <>
