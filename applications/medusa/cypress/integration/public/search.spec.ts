c9f72db1a08860d1c848db96a85c46d08472b867
Ilya <ilya.tkachov@hotmail.com>
2022-10-15 14:14:35 -0400

    test fix

--- applications/medusa/cypress/integration/public/search.spec.ts
+++ applications/medusa/cypress/integration/public/search.spec.ts
@@ -1,4 +1,4 @@
-import { clickOnButton, typeIntoPlaceholder } from '../../support/user_actions'
+import { typeIntoPlaceholder } from '../../support/user_actions'
 
 const series1 = 'Pilots Of A Star'
 const series2 = 'Heroes And Companions'
@@ -9,22 +9,11 @@ const club = 'Second Test Club'
 
 Cypress.config('defaultCommandTimeout', 20000)
 
-const openSearch = (): void => {
-  cy.get('button[aria-label="Open Search"]').should('be.visible').should('not.be.disabled').click({ force: true })
-  cy.findByPlaceholderText(/Search for a club/).should('be.visible')
-}
-
 describe('Search', () => {
-  it('search button', () => {
-    cy.visit('/clubs/discover')
-    openSearch()
-
-    // test closing
-    clickOnButton('Close')
-    cy.findByPlaceholderText(/Search for a club/).should('not.exist')
+  it('Search', () => {
+    cy.visit('/search')
 
     // test clicking on a character
-    openSearch()
     typeIntoPlaceholder(/Search for a club/, character1)
     cy.findByText('Search Results').should('be.visible')
     cy.findAllByText(character1).should('not.be.disabled').first().click({ force: true })
@@ -33,8 +22,7 @@ describe('Search', () => {
     cy.findByText('Series').should('be.visible')
 
     // test clicking on a category
-    cy.visit('/clubs/discover')
-    openSearch()
+    cy.visit('/search')
     typeIntoPlaceholder(/Search for a club/, category1)
     cy.findByText('Search Results').should('be.visible')
     cy.findAllByText(category1).should('not.be.disabled').first().click({ force: true })
@@ -43,24 +31,21 @@ describe('Search', () => {
     cy.findByText('Category').should('be.visible')
 
     // test clicking on a series
-    cy.visit('/clubs/discover')
-    openSearch()
+    cy.visit('/search')
     typeIntoPlaceholder(/Search for a club/, series2)
     cy.findByText('Search Results').should('be.visible')
     cy.findAllByText(series2).should('not.be.disabled').first().click({ force: true })
     cy.findByText('Series').should('be.visible')
 
     // test search bar
-    cy.visit('/clubs/discover')
-    openSearch()
+    cy.visit('/search')
     typeIntoPlaceholder(/Search for a club/, 'Second Test Club')
     cy.findByText('Search Results').should('be.visible')
     cy.findAllByText(club).should('not.be.disabled').first().click({ force: true })
     cy.url().should('contain', '/SecondTestClub')
 
     // test search bar no results
-    cy.visit('/clubs/discover')
-    openSearch()
+    cy.visit('/search')
     typeIntoPlaceholder(/Search for a club/, '123123123123123123123')
     cy.findByText(/We couldn't find a club/).should('be.visible')
   })
