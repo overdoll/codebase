6647f227cd860e3dccb130f8815a8f8121a5c843
Ilya <ilya.tkachov@hotmail.com>
2022-11-08 11:05:09 -0500

    fix skip e2e

--- applications/medusa/src/modules/hooks/useFeatureFlag.tsx
+++ applications/medusa/src/modules/hooks/useFeatureFlag.tsx
@@ -4,10 +4,10 @@ import posthog from 'posthog-js'
 type FeatureFlag = string | boolean | undefined
 
 export default function useFeatureFlag (flag: string): FeatureFlag {
-  const isInProperHostName = [process.env.NEXT_PUBLIC_POSTHOG_DOMAIN as string].includes(window.location.hostname)
   const [featureFlag, setFeatureFlag] = useState<FeatureFlag>(undefined)
 
   useEffect(() => {
+    const isInProperHostName = [process.env.NEXT_PUBLIC_POSTHOG_DOMAIN as string].includes(window.location.hostname)
     if (!isInProperHostName) return
     posthog?.onFeatureFlags(() => {
       setFeatureFlag(posthog?.getFeatureFlag(flag))
