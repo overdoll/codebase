1f97956ab224c13a1fa00351b6aaa85c0c9cef28
Ilya <ilya.tkachov@hotmail.com>
2022-11-10 11:56:25 -0500

    fixed artists not being able to edit post

--- applications/medusa/src/domain/club/create-post/RootCreatePost/DisposeCreatePost/ResultCreatePost/ResultCreatePost.tsx
+++ applications/medusa/src/domain/club/create-post/RootCreatePost/DisposeCreatePost/ResultCreatePost/ResultCreatePost.tsx
@@ -59,7 +59,7 @@ export default function ResultCreatePost (props: Props): JSX.Element {
   }
 
   // If the post was already submitted
-  if (queryData.post != null && queryData.post?.state !== 'DRAFT' && ((queryData.viewer?.isStaff) === false) && !(queryData.viewer?.isWorker)) {
+  if (queryData.post != null && queryData.post?.state !== 'DRAFT' && ((queryData.viewer?.isStaff) === false) && !(queryData.viewer?.isWorker) && !queryData?.club?.viewerIsOwner) {
     return (
       <MobileContainer pt={2}>
         <PostPlaceholder>
